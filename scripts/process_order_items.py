33f82fadf87662ec0babad15d18373384647c4c9
emmanuel24699 <emmanuel.gligbe@amalitechtraining.org>
2025-06-26 19:17:09 +0000

    Initialize AWS Glue job context and parse job parameters

---
+++ scripts/process_order_items.py
@@ -0,0 +1,32 @@
+import sys
+import boto3
+import pandas as pd
+from io import BytesIO
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lit, to_timestamp
+from delta.tables import DeltaTable
+
+# --- Initializations ---
+
+args = getResolvedOptions(
+    sys.argv, ["JOB_NAME", "S3_INPUT_PATH", "S3_PROCESSED_ZONE", "S3_REJECTED_PATH"]
+)
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# --- Script Parameters ---
+
+s3_input_path = args["S3_INPUT_PATH"]
+s3_processed_zone = args["S3_PROCESSED_ZONE"]
+s3_rejected_path = args["S3_REJECTED_PATH"]
+
+order_items_delta_path = f"{s3_processed_zone}order_items/"
